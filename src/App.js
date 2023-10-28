dad148d0cb143052543dfffa9ff78976177206c7
prakritishree <prakritishree027@gmail.com>
2023-10-28 22:49:04 +0530

    adding files to staging area

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import First from './First';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome page</h1>
+      <First/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/First.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';
+
+function First(){
+    return(
+        <div className="First">
+            <center><h1>My First web page using React</h1></center>
+            <h2>By Prakriti Shree</h2>
+            <p>This is a completely new experience for me.<br>
+            </br>Feeling exited to see my code run!!</p>
+            <h3>7-day Bootcamp</h3>
+            <p>Day-1 : React</p>
+        </div>
+    );
+}
+export default First;
